11b4890d9b7823313f3164f0c9f7b6b810a9efdb
Simon <simon.schmols@uni-oldenburg.de>
2024-12-01 23:39:41 +0100

    proof of concept for animations

--- backend/src/testing.py
+++ backend/src/testing.py
@@ -3,6 +3,8 @@ from PIL.Image import alpha_composite
 from fastapi import FastAPI, UploadFile, File, Form, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse, JSONResponse
+
+from backend.src.image_processing import calcBackgroundBleed, calcDropshadow
 from image_processing import process_image_blur
 from PIL import Image,ImageDraw,ImageFont,ImageChops
 from letter_rendering import generate_letter_layer
@@ -18,13 +20,25 @@ import base64
 from letter_rendering import generate_letter_mask, texture_letter
 
 def main():
-
+    # Proof of concept animation
     images = load_images_from_folder("C:/Users/Simon/Downloads/examplePhotos")
 
-    img = generate_letter_layer("Test", 200, images)
+    frames = []
+    for index in range(0, 180):
+        layer2 = generate_letter_layer("Video", 300, images)
+        layer0 = calcBackgroundBleed(layer2, index, 80, 300)
+        layer1 = calcDropshadow(layer2, 10, index-50, "#000000", 300)
+        full_frame = alpha_composite(layer0, layer1)
+        full_frame2 = alpha_composite(full_frame, layer2)
+        frames.append(full_frame2)
 
-    imgplot = plt.imshow(img)
-    plt.show()
+    video = cv2.VideoWriter('output_video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (frames[0].width, frames[0].height))
+    for frame in frames:
+        video.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
+
+    video.release()
+
+
 
 def load_images_from_folder(folder_path):
     images = []
